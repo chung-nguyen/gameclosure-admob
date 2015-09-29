2ad36566c2d68b1a16b542b760c1a42a8a3d745d
Chung <nguyentruongchung@gmail.com>
2015-09-29 23:57:25 +0700

    - missing commit

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ var admob = Class(function () {
     this.showAdView = function (opts) {
         NATIVE.plugins.sendEvent("AdmobPlugin", "showAdView", JSON.stringify(opts));
     }
-    
+
     this.hideAdView = function () {
         NATIVE.plugins.sendEvent("AdmobPlugin", "hideAdView", JSON.stringify({}));
     }
-    
+
     this.loadAdView = function () {
         NATIVE.plugins.sendEvent("AdmobPlugin", "loadAdView", JSON.stringify({}));
     }
